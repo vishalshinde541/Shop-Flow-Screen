f7382c463fcc3ac58216cc6b7bb32ba82e8590e7
Vishal Shinde <vishalshinde541@gmail.com>
2025-05-25 21:43:04 +0530

    added the resurces required for project like Font, Drawables etc

--- app/src/main/java/com/example/shopflowscreen/MainActivity.kt
+++ app/src/main/java/com/example/shopflowscreen/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shopflowscreen.ui.theme.AppFonts.CenturyOldStyleBold
+import com.example.shopflowscreen.ui.theme.AppFonts.NeuzeitSLTStdBook
+import com.example.shopflowscreen.ui.theme.AppFonts.NeuzeitSdBook
+import com.example.shopflowscreen.ui.theme.AppFonts.Tangerine
 import com.example.shopflowscreen.ui.theme.ShopFLowScreenTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +42,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = "Hello $name!",
+
+            )
+        // In your composables:
+        Text(
+            text = "CenturyOldStyleBold text",
+            fontFamily = CenturyOldStyleBold,
+            fontSize = 24.sp
+        )
+
+        Text(
+            text = "NeuzeitSLTStdBook text",
+            fontFamily = NeuzeitSLTStdBook,
+            fontSize = 36.sp
+        )
+
+        Text(
+            text = "NeuzeitSdBook text",
+            fontFamily = NeuzeitSdBook,
+            fontSize = 36.sp
+        )
+
+        Text(
+            text = "Tangerine text",
+            fontFamily = Tangerine,
+            fontSize = 20.sp
+        )
+
+        Image(
+            painter = painterResource(R.drawable.cart3),
+            contentDescription = "cart icon",
+            modifier = Modifier.size(100.dp)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/shopflowscreen/ui/theme/Font.kt
@@ -0,0 +1,15 @@
+package com.example.shopflowscreen.ui.theme
+
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.shopflowscreen.R
+
+object AppFonts {
+    val CenturyOldStyleBold = FontFamily(Font(R.font.century_old_style_std_bold, FontWeight.Bold))
+    val NeuzeitSLTStdBook = FontFamily(Font(R.font.neuzeit_slt_std_book, FontWeight.Normal))
+    val Tangerine = FontFamily(Font(R.font.tangerine, FontWeight.Normal))
+    val NeuzeitSdBook = FontFamily(Font(R.font.neuzeit_s_book, FontWeight.Normal))
+}
+
